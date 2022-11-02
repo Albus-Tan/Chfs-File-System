2a261828a071bca3b15c99483c0db96e8ccf3419
Albus_Tan <tzm0425@163.com>
2022-11-02 16:06:18 +0800

    finish lab2b part2

--- lock_server.cc
+++ lock_server.cc
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+pthread_mutex_t lock_server::mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t lock_server::cond = PTHREAD_COND_INITIALIZER;
+
 lock_server::lock_server():
   nacquire (0)
 {
@@ -24,7 +27,25 @@ lock_protocol::status
 lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
 {
   lock_protocol::status ret = lock_protocol::OK;
-	// Your lab2B part2 code goes here
+  // Your lab2B part2 code goes here
+
+  pthread_mutex_lock(&mutex); // lock to use lock_state_table
+  std::map<lock_protocol::lockid_t, lock_state>::iterator it = lock_state_table.find(lid);
+  if(it != lock_state_table.end()){
+    // first check if the lock is locked
+    // Threads should wait on a condition variable inside a loop that checks the boolean condition on which the thread is waiting
+    while(lock_state_table[lid] == lock_state::LOCKED){  // need to check again state of lock after leaving condition
+      // locked, handler should block until the lock is free
+      pthread_cond_wait(&cond, &mutex);  // wait for condition change
+    }
+    // free, modify to locked
+    lock_state_table[lid] = lock_state::LOCKED;
+  } else {
+    //  client asks for a lock that the server has never seen before
+    //  create the lock and grant it to the client
+    lock_state_table.insert(std::pair<lock_protocol::lockid_t, lock_state>(lid, lock_state::LOCKED));
+  }
+  pthread_mutex_unlock(&mutex);  // unlock mutex
   return ret;
 }
 
@@ -32,6 +53,18 @@ lock_protocol::status
 lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
 {
   lock_protocol::status ret = lock_protocol::OK;
-	// Your lab2B part2 code goes here
+  // Your lab2B part2 code goes here
+  pthread_mutex_lock(&mutex); // lock to use lock_state_table
+  std::map<lock_protocol::lockid_t, lock_state>::iterator it = lock_state_table.find(lid);
+  if(it == lock_state_table.end()){
+    // lock not found
+    ret = lock_protocol::NOENT;
+  } else {
+    // change the lock state to free
+    lock_state_table[lid] = lock_state::FREE;
+    // notify any threads that are waiting for the lock
+    pthread_cond_signal(&cond);
+  }
+  pthread_mutex_unlock(&mutex);  // unlock mutex
   return ret;
 }
--- lock_server.h
+++ lock_server.h
@@ -8,11 +8,21 @@
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
+#include <map>
 
 class lock_server {
 
  protected:
+  enum lock_state {
+    FREE = 0,  // no clients own the lock
+    LOCKED  // some client owns the lock
+  };
   int nacquire;
+  std::map<lock_protocol::lockid_t, lock_state> lock_state_table;
+
+  // a single pthreads mutex for all of lock_server
+  static pthread_mutex_t mutex;  // protect lock_state_table
+  static pthread_cond_t cond;
 
  public:
   lock_server();
